1991f5b4f4a15e053aaf9bc3e667d1d29ec6be6e
MinahilMJ <113772103+MinahilMJ@users.noreply.github.com>
2024-05-04 18:19:17 +0500

    Add files via upload

---
+++ PF_LAB_10/PF_Appetizer_LAB_10.cpp
@@ -0,0 +1,54 @@
+/*This C++ code prompts the user to enter the number of values they want to input, 
+reads in those values into an array using the `cin_Array` function, 
+and then outputs the contents of the array using the `cout_Array` function.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+void cin_Array(int arr[100], int n)
+{
+    
+	for(int a=0;a<n;a++)
+    
+	{
+        
+		cin>>arr[a];
+    
+	}
+
+}
+
+
+void cout_Array(const int arr[100],int n)
+
+{
+    
+	for(int b=0;b<n;b++)
+    
+	{
+        
+		cout<<arr[b];
+    
+	}
+
+}
+
+int main()
+
+{
+    
+	int n, arr[100];
+    
+	cout<<"Enter no Values ";
+    
+	cin>>n;
+    
+	cin_Array(arr,n);
+    
+	cout_Array(arr, n);
+    
+return 0;
+
+}
